12230f4058ca304be995ea3a90a178f050735e63
lilil-debian <lilil@lilil.at>
2017-03-21 19:01:13 +0100

    init

---
+++ src/Way/Generators/Commands/ModelGeneratorCommand.php
@@ -0,0 +1,115 @@
+<?php namespace Way\Generators\Commands;
+
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+
+class ModelGeneratorCommand extends GeneratorCommand {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'generate:model';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Generate a model';
+
+
+
+
+
+
+	/**
+	 * The path where the file will be created
+	 *
+	 * @return mixed
+	 */
+	protected function getFileGenerationPath()
+	{
+		$path = $this->getPathByOptionOrConfig('path', 'model_target_path');
+
+		return $path. '/' . ucwords($this->argument('modelName')) . '.php';
+	}
+
+	/**
+	 * Fetch the template data
+	 *
+	 * @return array
+	 */
+	protected function getTemplateData()
+	{
+
+		$fields = $this->option('fields');
+
+		$fields = preg_split('/\s?,\s/', $fields);
+		$parsed = [];
+		foreach($fields as $index => $field)
+		{
+			// Example:
+			// name:string:nullable => ['name', 'string', 'nullable']
+			// name:string(15):nullable
+			$chunks = preg_split('/\s?:\s?/', $field, null);
+
+			// The first item will be our property
+			$property = "'" . array_shift($chunks). "'\n\t\t\t\t\t\t\t";
+			echo $property . "\n";
+			array_push ( $parsed ,  $property );
+			// $parsed[$index] = $property;
+		}
+
+		$fillable = implode(",", $parsed);
+
+		return [
+			'NAME' => ucwords($this->argument('modelName')),
+			'FILLABLE'    =>  $fillable,
+			'TABLE'    =>  $this->argument('collection')
+		];
+	}
+
+	/**
+	 * Get path to the template for the generator
+	 *
+	 * @return mixed
+	 */
+	protected function getTemplatePath()
+	{
+		return $this->getPathByOptionOrConfig('templatePath', 'model_template_path');
+	}
+
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+	    return array(
+	        ['fields', null, InputOption::VALUE_OPTIONAL, 'Fields for the migration'],
+	        ['path', null, InputOption::VALUE_OPTIONAL, 'Where should the file be created?'],
+	        ['templatePath', null, InputOption::VALUE_OPTIONAL, 'The location of the template for this generator'],
+	        ['testing', null, InputOption::VALUE_OPTIONAL, 'For internal use only.']
+	    );
+	}
+
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return [
+			['modelName', InputArgument::REQUIRED, 'The name of the desired Eloquent model'],
+			['collection', InputArgument::REQUIRED, 'The name of the table'],
+		];
+	}
+
+}
